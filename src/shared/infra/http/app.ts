b59cc95cfd1ce2473271a6fa600d139922a0bf0a
joaomarcuscardoso <jm.cardoso@edu.udesc.br>
2021-10-24 14:55:12 -0300

    cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import "dotenv/config";
+import cors from "cors";
 import express, { Response, Request, NextFunction } from "express";
 import "express-async-errors";
 import swaggerUI from "swagger-ui-express";
@@ -21,7 +22,7 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
-
+app.use(cors());
 app.use(router);
 
 // error returned
